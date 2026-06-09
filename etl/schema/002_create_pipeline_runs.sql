87d717e0ce535352ed71d1c1e9e04f9473bf5d78
heni1123 <121931768+heni1123@users.noreply.github.com>
2026-06-09 13:47:29 +0200

    feat(etl): add generated pipeline files [run=39c31b7a]

--- etl/schema/002_create_pipeline_runs.sql
+++ etl/schema/002_create_pipeline_runs.sql
@@ -1,10 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS analytics;
 
 CREATE TABLE IF NOT EXISTS analytics.pipeline_runs (
-    run_id                              TEXT NOT NULL,
+    run_id                              SERIAL NOT NULL,
+    pipeline_code                       TEXT NOT NULL,
+    status                              TEXT NOT NULL,
     start_time                          TIMESTAMPTZ NOT NULL,
     end_time                            TIMESTAMPTZ,
-    status                              TEXT NOT NULL,
+    duration                            INTEGER,
     error_message                       TEXT,
     PRIMARY KEY (run_id)
 );
